09f0a63eb199f0a24fc854e8c340c454f527a056
Christopher Perrin <me@cperrin.xyz>
2021-01-14 14:11:15 +0100

    CNAME works now

--- cassandra.go
+++ cassandra.go
@@ -2,17 +2,19 @@ package cassandra
 
 import (
 	"github.com/coredns/coredns/plugin"
+	"github.com/coredns/coredns/plugin/pkg/upstream"
 	"github.com/coredns/coredns/request"
 	"github.com/gocql/gocql"
 	"github.com/miekg/dns"
 	"golang.org/x/net/context"
 	"net"
 	"strconv"
+	"strings"
 )
 
 type Cassandra struct {
-	Next         plugin.Handler
-	ClusterHosts []string
+	Next            plugin.Handler
+	ClusterHosts    []string
 	ClusterKeyspace string
 }
 
@@ -34,50 +36,40 @@ func (c Cassandra) ServeDNS(ctx context.Context, writer dns.ResponseWriter, msg 
 	defer session.Close()
 	var data map[string]string
 	var ttl uint
-	iter := session.Query(`SELECT data, ttl FROM records WHERE name = ? and type = ?`, name, state.Type()).Iter()
-	for iter.Scan(&data, &ttl) {
-		rr := dns.TypeToRR[state.QType()]()
-		hdr := dns.RR_Header{Name: state.QName(), Rrtype: state.QType(), Class: state.QClass(), Ttl: uint32(ttl)}
-		switch rr := rr.(type) {
-		case *dns.SOA:
-			rr.Hdr = hdr
-			i, _ := strconv.ParseUint(data["expire"], 10, 32)
-			rr.Expire = uint32(i)
-			i, _ = strconv.ParseUint(data["refresh"], 10, 32)
-			rr.Refresh = uint32(i)
-			i, _ = strconv.ParseUint(data["minttl"], 10, 32)
-			rr.Minttl = uint32(i)
-			i, _ = strconv.ParseUint(data["serial"], 10, 32)
-			rr.Serial = uint32(i)
-			i, _ = strconv.ParseUint(data["retry"], 10, 32)
-			rr.Retry = uint32(i)
-			rr.Mbox = data["mail"]
-			rr.Ns = data["primary"]
-			break
-		case *dns.A:
-			rr.Hdr = hdr
-			rr.A = net.ParseIP(data["address"])
-			break
-		case *dns.AAAA:
-			rr.Hdr = hdr
-			rr.AAAA = net.ParseIP(data["address"])
-			break
-		case *dns.TXT:
-			rr.Hdr = hdr
-			rr.Txt = []string{data["txt"]}
-			break
-		case *dns.CNAME:
-			rr.Hdr = hdr
-			rr.Target = data["target"]
-		case *dns.NS:
-			rr.Hdr = hdr
-			rr.Ns = data["ns"]
-		case *dns.PTR:
-			rr.Hdr = hdr
-			rr.Ptr = data["ptr"]
-		}
+	var rrType string
+	iter := session.Query(`SELECT data, ttl, type FROM records WHERE name = ? and type IN (?,'CNAME')`, name, state.Type()).Iter()
+	for iter.Scan(&data, &ttl, &rrType) {
+
+		rrQType := dns.StringToType[rrType]
+		rr := c.createRR(state.QName(), rrQType, data, ttl, state)
 
 		a.Answer = append(a.Answer, rr)
+
+		rrType := strings.ToUpper(rrType)
+		if rrType == "CNAME" && rrType != state.Type() {
+			a2 := make([]dns.RR, 0)
+			iter2 := session.Query(`SELECT data, ttl, type FROM records WHERE name = ? and type IN (?,'CNAME')`, data["target"], state.Type()).Iter()
+			var data2 map[string]string
+			var ttl2 uint
+			var rrType2 string
+			for iter2.Scan(&data2, &ttl2, &rrType2) {
+				rrQType := dns.StringToType[rrType2]
+				rr := c.createRR(data["target"], rrQType, data2, ttl2, state)
+				a2 = append(a2, rr)
+			}
+
+			if len(a2) != 0 {
+				a.Answer = append(a.Answer, a2...)
+			}
+			up := upstream.New()
+			msg, err := up.Lookup(ctx, state, data["target"], state.QType())
+			if err != nil {
+				plugin.Error("cassandra", err)
+			} else {
+				a.Answer = append(a.Answer, msg.Answer...)
+			}
+		}
+
 	}
 
 	if len(a.Answer) == 0 {
@@ -88,3 +80,47 @@ func (c Cassandra) ServeDNS(ctx context.Context, writer dns.ResponseWriter, msg 
 }
 
 func (c Cassandra) Name() string { return "cassandra" }
+
+func (c Cassandra) createRR(name string, rrQType uint16, data map[string]string, ttl uint, state request.Request) dns.RR {
+	rr := dns.TypeToRR[rrQType]()
+	hdr := dns.RR_Header{Name: name, Rrtype: rrQType, Class: state.QClass(), Ttl: uint32(ttl)}
+	switch rr := rr.(type) {
+	case *dns.SOA:
+		rr.Hdr = hdr
+		i, _ := strconv.ParseUint(data["expire"], 10, 32)
+		rr.Expire = uint32(i)
+		i, _ = strconv.ParseUint(data["refresh"], 10, 32)
+		rr.Refresh = uint32(i)
+		i, _ = strconv.ParseUint(data["minttl"], 10, 32)
+		rr.Minttl = uint32(i)
+		i, _ = strconv.ParseUint(data["serial"], 10, 32)
+		rr.Serial = uint32(i)
+		i, _ = strconv.ParseUint(data["retry"], 10, 32)
+		rr.Retry = uint32(i)
+		rr.Mbox = data["mail"]
+		rr.Ns = data["primary"]
+		break
+	case *dns.A:
+		rr.Hdr = hdr
+		rr.A = net.ParseIP(data["address"])
+		break
+	case *dns.AAAA:
+		rr.Hdr = hdr
+		rr.AAAA = net.ParseIP(data["address"])
+		break
+	case *dns.TXT:
+		rr.Hdr = hdr
+		rr.Txt = []string{data["txt"]}
+		break
+	case *dns.CNAME:
+		rr.Hdr = hdr
+		rr.Target = data["target"]
+	case *dns.NS:
+		rr.Hdr = hdr
+		rr.Ns = data["ns"]
+	case *dns.PTR:
+		rr.Hdr = hdr
+		rr.Ptr = data["ptr"]
+	}
+	return rr
+}
--- cassandra_test.go
+++ cassandra_test.go
@@ -3,31 +3,134 @@ package cassandra
 import (
 	"bytes"
 	"context"
-	"fmt"
 	"github.com/coredns/coredns/plugin/pkg/dnstest"
 	"github.com/coredns/coredns/plugin/test"
+	"github.com/gocql/gocql"
 	"github.com/miekg/dns"
 	golog "log"
 	"testing"
+	"time"
 )
 
-func TestCassandra(t *testing.T) {
-	// Create a new Cassandra Plugin. Use the test.ErrorHandler as the next plugin.
-	x := Cassandra{Next: test.ErrorHandler()}
+func setupDB() {
+	cluster := gocql.NewCluster("127.0.0.1")
+	cluster.Consistency = gocql.One
+	session, _ := cluster.CreateSession()
 
-	// Setup a new output buffer that is *not* standard output, so we can check if
-	// example is really being printed.
+	session.Query(`DROP KEYSPACE coredns_test`).Exec()
+	if err := session.Query(`CREATE KEYSPACE coredns_test WITH replication = {'class':'SimpleStrategy', 'replication_factor' : 1};`).Exec(); err != nil {
+		panic(err)
+	}
+	if err := session.Query(`create table coredns_test.records
+(
+    name text,
+    type text,
+    id timeuuid,
+    data map<text, text>,
+    "in" text,
+    ttl int,
+    primary key ((name, type), id)
+)
+    with caching = {'keys': 'ALL', 'rows_per_partition': 'NONE'}
+     and compaction = {'class': 'org.apache.cassandra.db.compaction.SizeTieredCompactionStrategy', 'max_threshold': '32', 'min_threshold': '4'}
+     and compression = {'chunk_length_in_kb': '64', 'class': 'org.apache.cassandra.io.compress.LZ4Compressor'}
+     and dclocal_read_repair_chance = 0.1;`).Exec(); err != nil {
+		panic(err)
+	}
+	if err := session.Query(`INSERT INTO coredns_test.records (name, type, id, data, "in", ttl) VALUES ('example.org.', 'A', now(), {'address': '127.0.0.1'}, 'IN', 30);`).Exec(); err != nil {
+		panic(err)
+	}
+	if err := session.Query(`INSERT INTO coredns_test.records (name, type, id, data, "in", ttl) VALUES ('example.org.', 'AAAA', now(), {'address': '::1'}, 'IN', 30);`).Exec(); err != nil {
+		panic(err)
+	}
+	if err := session.Query(`INSERT INTO coredns_test.records (name, type, id, data, "in", ttl) VALUES ('example.org.', 'AAAA', now(), {'address': '::2'}, 'IN', 30);`).Exec(); err != nil {
+		panic(err)
+	}
+	if err := session.Query(`INSERT INTO coredns_test.records (name, type, id, data, "in", ttl) VALUES ('txt.example.org.', 'TXT', now(), {'txt': 'Example'}, 'IN', 30);`).Exec(); err != nil {
+		panic(err)
+	}
+	if err := session.Query(`INSERT INTO coredns_test.records (name, type, id, data, "in", ttl) VALUES ('example.org.', 'SOA', now(), {'expire': '1800', 'mail': 'hostmaster.example.org.', 'minttl': '600', 'primary': 'ns01.example.org.', 'refresh': '3600', 'retry': '1800', 'serial': '1'}, 'IN', 30);`).Exec(); err != nil {
+		panic(err)
+	}
+	if err := session.Query(`INSERT INTO coredns_test.records (name, type, id, data, "in", ttl) VALUES ('cname.example.org.', 'CNAME', now(), {'target': 'example.org.'}, 'IN', 30);`).Exec(); err != nil {
+		panic(err)
+	}
+}
+
+func tearDownDB() {
+	cluster := gocql.NewCluster("127.0.0.1")
+	cluster.Timeout = time.Second * 30
+	session, _ := cluster.CreateSession()
+
+	if err := session.Query(`DROP KEYSPACE coredns_test`).Exec(); err != nil {
+		panic(err)
+	}
+}
+
+func TestCassandra(t *testing.T) {
+	setupDB()
+	defer tearDownDB()
+	tests := []struct {
+		qname        string
+		qtype        uint16
+		wantRetCode  int
+		wantAnswer   []string
+		wantMsgRCode int
+		wantNS       []string
+		expectedErr  error
+	}{
+		{
+			qname:      "example.org.",
+			qtype:      dns.TypeA,
+			wantAnswer: []string{"example.org.	30	IN	A	127.0.0.1"},
+		},
+		{
+			qname: "example.org.",
+			qtype: dns.TypeAAAA,
+			wantAnswer: []string{
+				"example.org.	30	IN	AAAA	::1",
+				"example.org.	30	IN	AAAA	::2",
+			},
+		},
+		{
+			qname:      "cname.example.org.",
+			qtype:      dns.TypeCNAME,
+			wantAnswer: []string{"cname.example.org.	30	IN	CNAME	example.org."},
+		},
+		{
+			qname: "cname.example.org.",
+			qtype: dns.TypeA,
+			wantAnswer: []string{
+				"cname.example.org.	30	IN	CNAME	example.org.",
+				"example.org.	30	IN	A	127.0.0.1",
+			},
+		},
+	}
+
+	cassandraTest := Cassandra{Next: test.ErrorHandler(), ClusterHosts: []string{"127.0.0.1"}, ClusterKeyspace: "coredns_test"}
+
 	b := &bytes.Buffer{}
 	golog.SetOutput(b)
 
 	ctx := context.TODO()
-	r := new(dns.Msg)
-	r.SetQuestion("example.com", dns.TypeAAAA)
-	// Create a new Recorder that captures the result, this isn't actually used in this test
-	// as it just serves as something that implements the dns.ResponseWriter interface.
-	rec := dnstest.NewRecorder(&test.ResponseWriter{})
 
-	// Call our plugin directly, and check the result.
-	x.ServeDNS(ctx, rec, r)
-	fmt.Println(rec)
+	for ti, tc := range tests {
+		r := new(dns.Msg)
+		r.SetQuestion(tc.qname, tc.qtype)
+
+		rec := dnstest.NewRecorder(&test.ResponseWriter{})
+
+		cassandraTest.ServeDNS(ctx, rec, r)
+
+		if len(tc.wantAnswer) != len(rec.Msg.Answer) {
+			t.Errorf("Test %d: Unexpected number of Answers. Want: %d, got: %d", ti, len(tc.wantAnswer), len(rec.Msg.Answer))
+		} else {
+			for i, gotAnswer := range rec.Msg.Answer {
+				if gotAnswer.String() != tc.wantAnswer[i] {
+					t.Errorf("Test %d: Unexpected answer.\nWant:\n\t%s\nGot:\n\t%s", ti, tc.wantAnswer[i], gotAnswer)
+				}
+			}
+		}
+
+	}
 }
